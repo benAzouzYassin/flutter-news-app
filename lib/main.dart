8920862ff30d40eb54179ee89604f34233c42dcd
yassine ben azouz <yassinebenazouz123@gmail.com>
2023-03-26 22:41:04 +0200

    cleaned the code

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Challenge',
+      title: 'News app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
