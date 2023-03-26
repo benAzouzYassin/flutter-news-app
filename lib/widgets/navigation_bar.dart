2f7f20b1f6e8c0073307a406f34c163fe19a6cd5
yassine ben azouz <yassinebenazouz123@gmail.com>
2023-03-26 22:21:49 +0200

    finished working on the bottom navigation bar

--- lib/widgets/navigation_bar.dart
+++ lib/widgets/navigation_bar.dart
@@ -2,43 +2,42 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:google_nav_bar/google_nav_bar.dart';
+
 class NavBar extends StatelessWidget {
   final int selected;
   const NavBar({super.key, required this.selected});
 
   @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      destinations: [
-        NavigationDestination(
-            icon: Icon(
-              Icons.home_filled,
-              color: Colors.grey,
-            ),
-            label: ""),
-        NavigationDestination(
-            icon: Icon(
-              Icons.language,
-              color: Colors.grey,
-            ),
-            label: ""),
-        NavigationDestination(
-            icon: Container(
-              child: Image.asset(
-                "lib/icons/save.png",
-                width: 20,
-                color: Colors.grey,
-              ),
-            ),
-            label: ""),
-        NavigationDestination(
-            icon: Icon(
-              Icons.person_outline_outlined,
-              color: Colors.grey,
-            ),
-            label: "")
-      ],
-      selectedIndex: selected,
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: GNav(
+        tabs: const [
+          GButton(
+            icon: Icons.home_filled,
+            text: "Home",
+          ),
+          GButton(
+            icon: Icons.language,
+            text: "Global",
+          ),
+          GButton(
+            icon: Icons.save,
+            text: "Saved",
+          ),
+          GButton(
+            icon: Icons.person_outline_outlined,
+            text: "Profile",
+          ),
+        ],
+        color: Colors.grey,
+        activeColor: Colors.white,
+        padding: EdgeInsets.all(5),
+        gap: 5,
+        tabBackgroundColor: Colors.blue,
+        selectedIndex: selected,
+      ),
     );
   }
 }
